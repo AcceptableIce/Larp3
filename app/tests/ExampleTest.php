5b6847be7b9c964a765528fe52b18ab785876813
May Roussel <may@maybreak.com>
2016-02-09 15:06:35 -0600

    Removed example test.

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -2,16 +2,4 @@
 
 class ExampleTest extends TestCase {
 
-	/**
-	 * A basic functional test example.
-	 *
-	 * @return void
-	 */
-	public function testBasicExample()
-	{
-		$crawler = $this->client->request('GET', '/');
-
-		$this->assertTrue($this->client->getResponse()->isOk());
-	}
-
 }
