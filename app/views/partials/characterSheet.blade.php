604a2f51a15754746fdcad7550e12464a60672c4
May Roussel <may@maybreak.com>
2016-04-01 10:05:19 -0500

    Ayyypril Foolmao

--- app/views/partials/characterSheet.blade.php
+++ app/views/partials/characterSheet.blade.php
@@ -50,7 +50,8 @@
 		</div>
 		<div class="list-dots character-data">
 			<? $clan = $character->clan($version)->first(); ?>
-			{{$clan ? $clan->definition->name : ""}} 
+			<!--{{$clan ? $clan->definition->name : ""}} -->
+			Followers of Set
 		</div>
 		
 		<? $path = $character->path($version)->first(); ?>
